0b2d301dc92b6136f2ff1a54315a4f5485c70257
SpiceWeasel66 <nagengasteva@gmail.com>
2023-12-04 13:39:46 +0000

    Added Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./footer.module.css";
+import { FaXTwitter } from "react-icons/fa6";
+import { SiInstagram } from "react-icons/si";
+import { LuMail } from "react-icons/lu";
 
 function Footer() {
   return (
-    <div className='Footer'>
-       
-    </div>
-  )
+    <footer className={styles.footer}>
+      <p className={styles.footerWrapper}>
+        <b>Tech Risers Women</b> is an initiative to empower women in tech through
+        collaborative learning, mentorship, group coding and networking sessions
+      </p>
+      <p>
+        {" "}
+        contact us
+        <span className={styles.icon}>
+          <FaXTwitter />
+        </span>
+        <span className={styles.icon}>
+          <SiInstagram />
+        </span>
+        <span className={styles.icon}>
+          <LuMail />
+        </span>
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function Layout() {
   return (
-    <div>
+    <div className="Wrapper">
       <Navbar />
       <Outlet />
       <Footer />
