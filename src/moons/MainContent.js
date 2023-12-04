23e09566419bc206dd2c58e230454c2b4bcc1cdb
SpiceWeasel66 <nagengasteva@gmail.com>
2023-12-04 19:32:36 +0000

    changed grid layout

--- src/moons/MainContent.js
+++ src/moons/MainContent.js
@@ -6,37 +6,30 @@ import mainLinkImage3 from "../media/melbaMouton.png";
 
 function MainContent() {
   return (
-    <div className={styles.mainContent}>
-      <div className={styles.content}>
-        <img src={mainLinkImage} alt="" className={styles.mainLinkImage} />
-        <p className={styles.mainLinkDescription}>
-          {" "}
-          With our initiatives, we unite women who code, encourage collaboration
-          and community.
-        </p>
-        <a href="/" className={styles.mainLink}>
-          learn more about our initiatives
-        </a>
-      </div>
-      <div className={styles.content}>
-        <img src={mainLinkImage2} alt="" className={styles.mainLinkImage} />{" "}
-        <p className={styles.mainLinkDescription}>
-          Every Wednesday we we come together to learn, share skills and and
-          support each other.
-        </p>
-        <a href="/" className={styles.mainLink}>
-          join our group coding sessions
-        </a>
-      </div>
-      <div className={styles.content}>
-        <img src={mainLinkImage3} alt="" className={styles.mainLinkImage} />
-        <p className={styles.mainLinkDescription}>
-          Help us to support women in tech and to follow our vision.
-        </p>
-        <a href="/" className={styles.mainLink}>
-          donate to Tech Risers Women
-        </a>
-      </div>
+    <div className={styles.mainContentWrapper}>
+      <img src={mainLinkImage} alt="" className={styles.mainLinkImage} />
+      <img src={mainLinkImage2} alt="" className={styles.mainLinkImage} />
+      <img src={mainLinkImage3} alt="" className={styles.mainLinkImage} />
+      <p className={styles.mainLinkDescription}>
+        With our initiatives, we unite women who code, encourage collaboration
+        and community.
+      </p>
+      <p className={styles.mainLinkDescription}>
+        Every Wednesday we we come together to learn, share skills and and
+        support each other.
+      </p>{" "}
+      <p className={styles.mainLinkDescription}>
+        Help us to support women in tech and to follow our vision.
+      </p>
+      <a href="/" className={styles.mainLink}>
+        learn more about our initiatives
+      </a>
+      <a href="/" className={styles.mainLink}>
+        join our group coding sessions
+      </a>
+      <a href="/" className={styles.mainLink}>
+        donate to Tech Risers Women
+      </a>
     </div>
   );
 }
