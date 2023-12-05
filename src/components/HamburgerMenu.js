ab0c6952f21ac5a27b379d435d13db2ea39fa33f
SpiceWeasel66 <nagengasteva@gmail.com>
2023-12-05 23:34:47 +0000

    new component for Navbar + ease transition opening

---
+++ src/components/HamburgerMenu.js
@@ -0,0 +1,93 @@
+import React, { useState, useRef, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./hamburgerMenu.module.css";
+
+function HamburgerMenu() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const toggleNavbar = () => setNavbarOpen(!navbarOpen);
+  const closeNavbar = () => setNavbarOpen(false);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (navbarOpen && ref.current && !ref.current.contains(event.target)) {
+        setNavbarOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [navbarOpen]);
+
+  return (
+    <div className={styles.hamburgerMenu} ref={ref}>
+      {/* Hamburger Button */}
+      <button className={styles.hamburgerButton} onClick={toggleNavbar}>
+        <div
+          className={
+            navbarOpen ? styles.hamburgerTopOpen : styles.hamburgerTopClosed
+          }
+        ></div>
+        <div
+          className={
+            navbarOpen ? styles.hamburgerMidOpen : styles.hamburgerMidClosed
+          }
+        ></div>
+        <div
+          className={
+            navbarOpen
+              ? styles.hamburgerBottomOpen
+              : styles.hamburgerBottomClosed
+          }
+        ></div>
+      </button>
+
+      <div className={navbarOpen ? styles.menu : styles.menuClosed}>
+        {/* Menu */}
+        <ul className={`${styles.smallScreenMenu} ${styles.open}`}>
+          <li className={styles.hamburgerLink}>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              onClick={closeNavbar}
+            >
+              Home
+            </NavLink>{" "}
+          </li>
+          <li className={styles.hamburgerLink}>
+            <NavLink
+              to="about"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              onClick={closeNavbar}
+            >
+              About
+            </NavLink>
+          </li>
+          <li className={styles.hamburgerLink}>
+            <NavLink
+              to="donate"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              onClick={closeNavbar}
+            >
+              Donate
+            </NavLink>
+          </li>
+          <li className={styles.hamburgerLink}>
+            <NavLink
+              to="contact"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              onClick={closeNavbar}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default HamburgerMenu;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,64 +1,26 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./navbar.module.css";
 import Logo from "../media/logodark.png";
+import MobileMenu from "./HamburgerMenu.js";
 
-const Navbar = () => {
-  const [navbarOpen, setNavbarOpen] = useState(false);
-  const toggleNavbar = () => {
-    setNavbarOpen(!navbarOpen);
-  };
-  const closeNavbar = () => {
-    setNavbarOpen(false);
-  };
-  const ref = useRef();
-  useEffect(() => {
-    const handler = (event) => {
-      if (navbarOpen && ref.current && !ref.current.contains(event.target)) {
-        setNavbarOpen(false);
-      }
-    };
-    document.addEventListener("mousedown", handler);
-
-    return () => {
-      document.removeEventListener("mousedown", handler);
-    };
-  }, [navbarOpen]);
-
+function Navbar() {
   return (
-    <nav ref={ref} className={styles.navbar}>
-      <button className={styles.hamburgerButton} onClick={toggleNavbar}>
-        <div
-          className={
-            navbarOpen ? styles.hamburgerTopOpen : styles.hamburgerTopClosed
-          }
-        />
-
-        <div
-          className={
-            navbarOpen ? styles.hamburgerMidOpen : styles.hamburgerMidClosed
-          }
-        />
-        <div
-          className={
-            navbarOpen ? styles.hamburgerBottomOpen : styles.hamburgerBottomClosed
-          }
-   
-        />
-      </button>
+    <nav className={styles.navbar}>
+      <div className={styles.mobileMenu}>
+        <MobileMenu />
+      </div>
       <NavLink
         to="/"
         className={({ isActive }) => (isActive ? styles.homeLogo : "")}
-        onClick={closeNavbar}
       >
         <img src={Logo} alt="Tech Risers Women Logo" />
       </NavLink>
-      <ul className={styles.bigScreenMenu}>
+      <ul className={styles.desktopMenu}>
         <li>
           <NavLink
             to="/"
             className={({ isActive }) => (isActive ? styles.active : "")}
-            onClick={closeNavbar}
           >
             Home
           </NavLink>
@@ -67,7 +29,6 @@ const Navbar = () => {
           <NavLink
             to="about"
             className={({ isActive }) => (isActive ? styles.active : "")}
-            onClick={closeNavbar}
           >
             About
           </NavLink>
@@ -76,7 +37,6 @@ const Navbar = () => {
           <NavLink
             to="donate"
             className={({ isActive }) => (isActive ? styles.active : "")}
-            onClick={closeNavbar}
           >
             Donate
           </NavLink>
@@ -85,55 +45,12 @@ const Navbar = () => {
           <NavLink
             to="contact"
             className={({ isActive }) => (isActive ? styles.active : "")}
-            onClick={closeNavbar}
           >
             Contact
           </NavLink>
         </li>
       </ul>
-
-      {/* smallscreen Menu  */}
-      {navbarOpen && (
-        <ul className={`${styles.smallScreenMenu} ${styles.open}`}>
-          <li className={styles.hamburgerLink}>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              onClick={closeNavbar}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className={styles.hamburgerLink}>
-            <NavLink
-              to="about"
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              onClick={closeNavbar}
-            >
-              About
-            </NavLink>
-          </li>
-          <li className={styles.hamburgerLink}>
-            <NavLink
-              to="donate"
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              onClick={closeNavbar}
-            >
-              Donate
-            </NavLink>
-          </li>
-          <li className={styles.hamburgerLink}>
-            <NavLink
-              to="contact"
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              onClick={closeNavbar}
-            >
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      )}
     </nav>
   );
-};
+}
 export default Navbar;
