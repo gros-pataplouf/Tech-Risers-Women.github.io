c92561920589b4dc4755b3b7e7b41a2bfff34e56
SpiceWeasel66 <nagengasteva@gmail.com>
2023-12-05 17:15:57 +0000

    floating headlines all of the pages

--- src/planets/Contact.js
+++ src/planets/Contact.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./contact.module.css";
 import { FaXTwitter } from "react-icons/fa6";
 import { SiInstagram } from "react-icons/si";
 import { LuMail } from "react-icons/lu";
 
 function Contact() {
+  const [isInViewport, setIsInViewport] = useState(false);
+  const contactHeroDividerRef = useRef(null);
+
+  useEffect(() => {
+    const handleIntersection = (entries) => {
+      const isIntersecting = entries[0].isIntersecting;
+      setIsInViewport(isIntersecting);
+    };
+
+    const observer = new IntersectionObserver(handleIntersection);
+
+    if (contactHeroDividerRef.current) {
+      observer.observe(contactHeroDividerRef.current);
+    }
+
+    return () => {
+      if (contactHeroDividerRef.current) {
+        observer.unobserve(contactHeroDividerRef.current);
+      }
+    };
+  });
   return (
     <div className={styles.contact}>
-      <div className={styles.contactHeroDivider}>
+      <div ref={contactHeroDividerRef} className={styles.contactHeroDivider}>
         <div>
-          <h1>Contact us!</h1>
+          <h1
+            className={`${styles.noContactHeadline} ${
+              isInViewport ? styles.contactHeadline : styles.noContactHeadline
+            }`}
+          >
+            Contact us!
+          </h1>
         </div>
       </div>
       <div className={styles.contactWrapper}>
         <p>Get in Touch </p>
         <p>
-          If you have any questions or would like to learn more about {" "}
+          If you have any questions or would like to learn more about{" "}
           <em>Tech Risers Women</em>, please don't hesitate to contact us. We
           also welcome curious women to join our Wednesday group sessions.
         </p>
--- src/planets/Donate.js
+++ src/planets/Donate.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./donate.module.css";
 import christineDarden from "../media/christineDarden.png";
 
 function Donate() {
+  const [isInViewport, setIsInViewport] = useState(false);
+  const donateHeroDividerRef = useRef(null);
+
+  useEffect(() => {
+    const handleIntersection = (entries) => {
+      const isIntersecting = entries[0].isIntersecting;
+      setIsInViewport(isIntersecting);
+    };
+
+    const observer = new IntersectionObserver(handleIntersection);
+
+    if (donateHeroDividerRef.current) {
+      observer.observe(donateHeroDividerRef.current);
+    }
+
+    return () => {
+      if (donateHeroDividerRef.current) {
+        observer.unobserve(donateHeroDividerRef.current);
+      }
+    };
+  });
   return (
     <div className={styles.donate}>
-      <div className={styles.donateHeroDivider}>
+      <div ref={donateHeroDividerRef} className={styles.donateHeroDivider}>
         <div>
-          <h1> Support us</h1>
+          <h1
+            className={`${styles.noDonateHeadline} ${
+              isInViewport ? styles.donateHeadline : styles.noDonateHeadline
+            }`}
+          >
+            Support us
+          </h1>
         </div>
       </div>
       <div className={styles.donateWrapper}>
