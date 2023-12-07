467a49e463e59b40ac3e1cf6b799214f3a0d4f1a
SpiceWeasel66 <nagengasteva@gmail.com>
2023-12-07 11:37:09 +0000

    slimmed down functions and css

--- src/moons/PillarsSmallScreen.js
+++ src/moons/PillarsSmallScreen.js
@@ -7,8 +7,8 @@ import { SlArrowDown } from "react-icons/sl";
 
 function Pillars() {
   const [info, setInfo] = useState(styles.pillarClosed);
-  const [info2, setInfo2] = useState(styles.pillar2Closed);
-  const [info3, setInfo3] = useState(styles.pillar3Closed);
+  const [info2, setInfo2] = useState(styles.pillarClosed);
+  const [info3, setInfo3] = useState(styles.pillarClosed);
   const [arrow, setArrow] = useState(styles.arrowClosed);
   const [arrow2, setArrow2] = useState(styles.arrowClosed);
   const [arrow3, setArrow3] = useState(styles.arrowClosed);
@@ -25,32 +25,28 @@ function Pillars() {
 
   const handleClick2 = () => {
     const newPosition2 =
-      info2 === styles.pillar2Closed
-        ? styles.pillar2Open
-        : styles.pillar2Closed;
+      info2 === styles.pillarClosed ? styles.pillarOpen : styles.pillarClosed;
     setInfo2(newPosition2);
 
     const newArrow2 =
-      arrow2 === styles.arrow2Closed ? styles.arrow2Open : styles.arrow2Closed;
+      arrow2 === styles.arrowClosed ? styles.arrowOpen : styles.arrowClosed;
     setArrow2(newArrow2);
   };
 
   const handleClick3 = () => {
     const newPosition3 =
-      info3 === styles.pillar3Closed
-        ? styles.pillar3Open
-        : styles.pillar3Closed;
+      info3 === styles.pillarClosed ? styles.pillarOpen : styles.pillarClosed;
     setInfo3(newPosition3);
 
     const newArrow3 =
-      arrow3 === styles.arrow3Closed ? styles.arrow3Open : styles.arrow3Closed;
+      arrow3 === styles.arrowClosed ? styles.arrowOpen : styles.arrowClosed;
     setArrow3(newArrow3);
   };
 
   return (
     <div className={styles.pillars}>
       {/* Pillar 1 */}
-      <div className={styles.pillar1} onClick={handleClick}>
+      <div className={styles.pillar} onClick={handleClick}>
         <img src={pillar1Image} alt="" className={styles.pillarImage} />
         <div className={`${styles.arrowClosed} ${arrow}`}>
           <SlArrowDown />
@@ -63,13 +59,13 @@ function Pillars() {
         to gain confidence, feel heard, and thrive with a sense of belonging
       </div>
       {/* Pillar 2 */}
-      <div className={styles.pillar2} onClick={handleClick2}>
+      <div className={styles.pillar} onClick={handleClick2}>
         <img src={pillar2Image} alt="" className={styles.pillarImage} />
-        <div className={`${styles.arrow2Closed} ${arrow2}`}>
+        <div className={`${styles.arrowClosed} ${arrow2}`}>
           <SlArrowDown />
         </div>
       </div>{" "}
-      <div className={`${styles.pillar2Closed} ${info2}`} onClick={handleClick2}>
+      <div className={`${styles.pillarClosed} ${info2}`} onClick={handleClick2}>
         The focus here is on providing tangible, hands-on skills that are
         directly applicable in the tech field. Workshops, training sessions, and
         collaborative projects are designed to equip women with the practical
@@ -79,13 +75,13 @@ function Pillars() {
         confidence and competence in their roles.
       </div>
       {/* Pillar 3 */}
-      <div className={styles.pillar3} onClick={handleClick3}>
+      <div className={styles.pillar} onClick={handleClick3}>
         <img src={pillar3Image} alt="" className={styles.pillarImage} />
-        <div className={`${styles.arrow3Closed} ${arrow3}`}>
+        <div className={`${styles.arrowClosed} ${arrow3}`}>
           <SlArrowDown />
         </div>
       </div>{" "}
-      <div className={`${styles.pillar3Closed} ${info3}`} onClick={handleClick3}>
+      <div className={`${styles.pillarClosed} ${info3}`} onClick={handleClick3}>
         We want to create an environment that supports continuous learning and
         mentorship to foster the professional development of women in tech. This
         involves providing resources, guidance, and opportunities to grow
