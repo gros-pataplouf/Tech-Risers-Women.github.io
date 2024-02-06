27c69dec26cd8e42d9bf14da934a6666d4df9275
SpiceWeasel66 <nagengasteva@gmail.com>
2024-02-06 18:22:14 +0000

    changed to react <Link>
    With GitHub pages root-relative link didn't work as expected local, changed to react router-dom "Link"
    
    changed link alt to title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-import React from "react";
-import styles from "./footer.module.css";
+import React from 'react';
+import styles from './footer.module.css';
 // import { FaXTwitter } from "react-icons/fa6";
 // import { SiInstagram } from "react-icons/si";
-import { LuMail } from "react-icons/lu";
-import { TbHeartCode } from "react-icons/tb";
+import {LuMail} from 'react-icons/lu';
+import {TbHeartCode} from 'react-icons/tb';
 
 function Footer() {
   return (
@@ -13,35 +13,37 @@ function Footer() {
         through collaborative learning, mentorship, group coding and networking
         sessions
       </p>
-      <p>       
+      <p>
         contact us
         <span className={styles.icon}>
-          <a  href="mailto:techriserswomen@gmail.com?subject=Tech Risers Women" 
-              target="_blank" 
-              rel="noreferrer" 
-              alt="send an Email to Tech Risers Women to get in touch"  
-              className={styles.contactLink}>
-                <LuMail />
+          <a
+            href='mailto:techriserswomen@gmail.com?subject=Tech Risers Women'
+            target='_blank'
+            rel='noreferrer'
+            title='send an Email to Tech Risers Women to get in touch'
+            className={styles.contactLink}>
+            <LuMail />
           </a>
         </span>
-      {/* <span className={styles.icon}>
-            <a href="/" alt="" className={styles.socialsLink}>
+        {/* <span className={styles.icon}>
+            <a href="/" title="" className={styles.socialsLink}>
             <FaXTwitter />
             </a>
           </span> */}
         {/* <span className={styles.icon}>
-          <a href="/" alt="" className={styles.socialsLink}>
+          <a href="/" title="" className={styles.socialsLink}>
           <SiInstagram />
           </a>
           </span> */}
-          get updates
-          <span className={styles.icon}>
-          <a  href="https://forms.gle/MqyXV2HPRtUMAopE6" 
-              target="_blank" 
-              rel="noreferrer" 
-              alt="Fill out this form to receive regular updates" 
-              className={styles.contactLink}>
-                <TbHeartCode />
+        get updates
+        <span className={styles.icon}>
+          <a
+            href='https://forms.gle/MqyXV2HPRtUMAopE6'
+            target='_blank'
+            rel='noreferrer'
+            title='Fill out this form to receive regular updates'
+            className={styles.contactLink}>
+            <TbHeartCode />
           </a>
         </span>
       </p>
--- src/moons/MainIntro.js
+++ src/moons/MainIntro.js
@@ -1,36 +1,42 @@
-import React from "react";
-import styles from "./mainIntro.module.css";
-import womenImage from "../media/MaryWilkes.png";
-
+import React from 'react';
+import {Link} from 'react-router-dom';
+import styles from './mainIntro.module.css';
+import womenImage from '../media/MaryWilkes.png';
 
 function intro() {
   return (
     <div className={styles.intro}>
       <div className={styles.introWrapper}>
-          <div>
-           <p>
+        <div>
+          <p>
             Fostering empowerment in the tech industry, our community-run
             initiative brings together women through regular meetings and group
             coding sessions. We provide a platform for collaborative learning,
             mentorship, and networking, creating a community that champions
             diversity and innovation. Join us as we inspire, support, and propel
             women in tech toward their professional goals
-           </p>
-        
-            <a href="/about" alt="learn more about Tech risers Women">
-              <button className={styles.aboutButton}>
-              learn more 
-              </button>
-            </a>
-               <a href="https://forms.gle/MqyXV2HPRtUMAopE6" target="_blank" rel="noreferrer" alt="sign up for updates about Tech risers Women">
-              <button className={styles.getUpdatesButton}>
-              get updates
-              </button>
-            </a>
-        
-          </div>
+          </p>
+
+          <Link
+            to='/about'
+            title='learn more about Tech risers Women'>
+            <button className={styles.aboutButton}>learn more</button>
+          </Link>
+
+          <a
+            href='https://forms.gle/MqyXV2HPRtUMAopE6'
+            target='_blank'
+            rel='noreferrer'
+            alt='sign up for updates about Tech risers Women'>
+            <button className={styles.getUpdatesButton}>get updates</button>
+          </a>
+        </div>
         <div>
-          <img src={womenImage} alt="Mary Allen Wilkes working on one of the first PCs" className={styles.introImage} />
+          <img
+            src={womenImage}
+            alt='Mary Allen Wilkes working on one of the first PCs'
+            className={styles.introImage}
+          />
         </div>
       </div>
     </div>
